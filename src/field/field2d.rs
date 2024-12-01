1b49978e072e7ac9447759734004a0ba39067b9b
Jonathan Samson <samsonjj@vcu.edu>
2024-12-01 14:20:28 -0500

    2d is hard

--- src/field/field2d.rs
+++ src/field/field2d.rs
@@ -11,7 +11,7 @@ pub struct Field2D {
 impl Field for Field2D {
     fn render(&self) {
         let pixel_width = 1.5;
-        let pixel_height = 40.0;
+        let pixel_height = 1.5;
         let offset_x = 50.0;
         let offset_y = 50.0;
 
@@ -32,9 +32,13 @@ impl Field for Field2D {
     }
     fn update(&mut self) {
         let mut field_deltas = vec![vec![0f32; self.width()]; self.height()];
-        for j in 0..self.width() {
-            field_deltas[0][j] += self.force(j, j as i32 + 1);
-            field_deltas[0][j] += self.force(j, j as i32 - 1);
+        for i in 0..self.height() {
+            for j in 0..self.width() {
+                field_deltas[i][j] += self.force((i, j), (j as i32 + 1, i as i32));
+                field_deltas[i][j] += self.force((i, j), (j as i32 - 1, i as i32));
+                field_deltas[i][j] += self.force((i, j), (j as i32, i as i32 - 1));
+                field_deltas[i][j] += self.force((i, j), (j as i32, i as i32 + 1));
+            }
         }
         // update volocities
         for i in 0..self.height() {
@@ -55,20 +59,19 @@ impl Field2D {
     pub fn new() -> Self {
         Self {
             // pixels: vec![vec![0f32; 16]]
-            // pixels: Self::pixels_centered(),
-            pixels: Self::pixels_at_end(),
+            pixels: Self::pixels_centered(),
+            // pixels: Self::pixels_at_end(),
         }
     }
     fn pixels_centered() -> Vec<Vec<Pixel>> {
-        let mut pixels = vec![vec![Pixel::zero(); 256]];
-        // let half = pixels[0].len() / 2;
+        let mut pixels = vec![vec![Pixel::zero(); 256]; 256];
         for j in 120..136 {
             pixels[0][j] = Pixel { u: 255.0, v: 0.0 };
         }
         pixels
     }
     fn pixels_at_end() -> Vec<Vec<Pixel>> {
-        let mut pixels = vec![vec![Pixel::zero(); 256]];
+        let mut pixels = vec![vec![Pixel::zero(); 256]; 256];
         // let half = pixels[0].len() / 2;
         for j in 0..20 {
             pixels[0][j] = Pixel {
@@ -80,13 +83,16 @@ impl Field2D {
     }
 
     // attempt to simulate the second derivative
-    fn force(&self, target: usize, source: i32) -> f32 {
-        if source < 0 || source >= self.width() as i32 {
+    fn force(&self, target: (usize, usize), source: (i32, i32)) -> f32 {
+        if source.0 < 0 || source.0 >= self.width() as i32 {
             return 0f32;
         }
-        let source = source as usize;
+        if source.1 < 0 || source.1 >= self.height() as i32 {
+            return 0f32;
+        }
+        let source = (source.0 as usize, source.1 as usize);
         // c^2
-        (0.05) * (self.pixels[0][source].u - self.pixels[0][target].u)
+        (0.05) * (self.pixels[source.1][source.0].u - self.pixels[target.1][target.0].u)
     }
     fn width(&self) -> usize {
         self.pixels[0].len()
--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ async fn main() {
     loop {
         clear_background(WHITE);
 
-        // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
-        // draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
         game.update();
         for observer in observers.iter_mut() {
             observer.update(&game);
